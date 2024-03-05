3321faa2908f278520e262ef532d2fd2b43430da
AjayKumarSaini8 <108985901+AjayKumarSaini8@users.noreply.github.com>
2024-03-05 15:32:25 +0530

    Token Authentication

--- py_client/create.py
+++ py_client/create.py
@@ -1,5 +1,6 @@
 import requests
 
+headers = {"Authorization": "Bearer 3c8cf90392edea3ffd2ed44b2b6a2b22230a2f19"}
 endpoint = "http://127.0.0.1:8000/api/products/"
 
 data = {
@@ -7,6 +8,6 @@ data = {
     "price": 12.54,
 }
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 
 print(get_response.json())
--- py_client/list.py
+++ py_client/list.py
@@ -13,7 +13,7 @@ print(auth_response.json())
 
 if auth_response.status_code == 200:
     token = auth_response.json()["token"]
-    headers = {"Authorization": f"Beare {token}"}
+    headers = {"Authorization": f"Bearer {token}"}
     endpoint = "http://127.0.0.1:8000/api/products/"
 
     get_response = requests.get(endpoint, headers=headers)
